121644bb0b2d36ea247dc569051e9fc5bc53eb53
AnilKavali <100416945+AnilKavali@users.noreply.github.com>
2022-02-25 18:17:34 +0530

    Add files via upload

---
+++ customerService/src/main/java/com/coforge/customerService/service/CusotmerService.java
@@ -0,0 +1,13 @@
+package com.coforge.customerService.service;
+
+import com.coforge.customerService.dto.Deposit;
+import com.coforge.customerService.entity.Customer;
+
+import java.util.List;
+
+public interface CusotmerService {
+    public Customer registerCustomer(Customer customer);
+    public List<Customer> customerDetails();
+    void deleteCustomerAccount(int id);
+   
+  }
