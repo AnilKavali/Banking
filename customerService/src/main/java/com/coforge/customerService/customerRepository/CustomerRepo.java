80e7d2e3775827e0379a69b1d3f0e5b3f7c153a7
AnilKavali <100416945+AnilKavali@users.noreply.github.com>
2022-03-08 17:12:39 +0530

    Delete customerService directory

--- customerService/src/main/java/com/coforge/customerService/customerRepository/CustomerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.coforge.customerService.customerRepository;
-
-import com.coforge.customerService.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer,Integer> {
-}
