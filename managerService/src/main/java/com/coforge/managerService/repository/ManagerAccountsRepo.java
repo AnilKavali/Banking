3b971b4aaa7c7be360c33896a321eadf3917776a
AnilKavali <100416945+AnilKavali@users.noreply.github.com>
2022-03-08 17:15:09 +0530

    Delete managerService directory

--- managerService/src/main/java/com/coforge/managerService/repository/ManagerAccountsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.coforge.managerService.repository;
-
-import com.coforge.managerService.entity.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerAccountsRepo extends JpaRepository<Accounts,String> {
-}
--- managerService/src/main/java/com/coforge/managerService/repository/ManagerTransactions.java
+++
@@ -1,7 +0,0 @@
-package com.coforge.managerService.repository;
-
-import com.coforge.managerService.entity.Transactions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerTransactions extends JpaRepository<Transactions,Integer> {
-}
